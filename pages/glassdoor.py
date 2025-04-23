4609ed06da275ab29d9df577abe0c5b245f744f9
KMAVBD485 <kim.vallesteros@payreto.com>
2025-04-23 22:17:58 +0800

    Update glassdoor.py

--- pages/glassdoor.py
+++ pages/glassdoor.py
@@ -5,6 +5,7 @@ import re
 import undetected_chromedriver as uc
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 
 st.set_page_config(page_title="Payreto Glassdoor Review Scraper", layout="centered")
